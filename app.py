cfe3400f9fef1c66150ed330d98cf3aa237ff423
copymary9170 <copymary9170@gmail.com>
2026-03-01 00:32:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-mport streamlit as st
+import streamlit as st
 import pandas as pd
 import sqlite3
 import numpy as np
@@ -7788,5 +7788,6 @@ def registrar_venta_global(
     finally:
         if conn_creada and conn_local is not None:
             conn_local.close()
+
 
 
