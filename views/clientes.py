b08d8e6096ede93eb738b1049d29325245b70665
copymary9170 <copymary9170@gmail.com>
2026-03-07 23:47:12 -0400

    Update clientes.py

--- views/clientes.py
+++ views/clientes.py
@@ -3,6 +3,6 @@ from modules.clientes import render_clientes as clientes_module
 
 
 def render_clientes(usuario):
-    st.title("Clientes")
+    st.title("👥 Clientes")
 
     clientes_module(usuario)
