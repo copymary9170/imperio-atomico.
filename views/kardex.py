ebda4e44687eb515ff6c8f7a32a9ec9bd6598cb7
copymary9170 <copymary9170@gmail.com>
2026-03-08 18:20:51 -0400

    Update kardex.py

--- views/kardex.py
+++ views/kardex.py
@@ -2,7 +2,8 @@ import streamlit as st
 from modules.inventario import render_kardex
 
 
-def render_kardex(usuario):
+def render_kardex_view(usuario: str):
+
     st.title("📊 Kardex")
 
     render_kardex(usuario)
