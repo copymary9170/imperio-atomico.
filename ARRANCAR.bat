81d3e871588856485616e5d3caa9ff3d0b6f10b0
copymary9170 <copymary9170@gmail.com>
2026-03-08 16:18:55 -0400

    Update ARRANCAR.bat

--- ARRANCAR.bat
+++ ARRANCAR.bat
@@ -1,3 +1,10 @@
 @echo off
+echo ===============================
+echo INICIANDO IMPERIO ERP
+echo ===============================
+
 py -m streamlit run app.py
+
+echo.
+echo Si hubo un error revisa arriba.
 pause
