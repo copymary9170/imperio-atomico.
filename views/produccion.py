9d203b2340e7a2d5710f786e950caf10f39ed9ca
copymary9170 <copymary9170@gmail.com>
2026-03-07 23:43:14 -0400

    Create produccion.py

---
+++ views/produccion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from modules.produccion import render_produccion as produccion_module
+
+
+def render_produccion(usuario):
+    st.title("Producción")
+
+    produccion_module(usuario)
