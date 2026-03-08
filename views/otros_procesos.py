edda860b64f58a052bad8831c60e9fb1c84a6267
copymary9170 <copymary9170@gmail.com>
2026-03-08 11:31:34 -0400

    Create otros_procesos.py

---
+++ views/otros_procesos.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from modules.procesos import render_otros_procesos
+
+
+def render_otros_procesos(usuario):
+    st.title("🛠️ Otros Procesos")
+
+    render_otros_procesos(usuario)
