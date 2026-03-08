06447516d2820660c12d1e712cf4aa8af66fe6e6
copymary9170 <copymary9170@gmail.com>
2026-03-07 23:41:47 -0400

    Create inventario.py

---
+++ views/inventario.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from modules.inventario import render_inventario as inventario_module
+
+
+def render_inventario(usuario):
+    st.title("Inventario")
+
+    inventario_module(usuario)
