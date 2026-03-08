e6cc2866b5932b8615d90f2d38b6d9532d37d747
copymary9170 <copymary9170@gmail.com>
2026-03-08 18:48:24 -0400

    Update cmyk.py

--- views/cmyk.py
+++ views/cmyk.py
@@ -2,7 +2,8 @@ import streamlit as st
 from modules.cmyk import render_cmyk
 
 
-def render_cmyk(usuario):
+def render_cmyk_view(usuario: str):
+
     st.title("🎨 Análisis CMYK")
 
     render_cmyk(usuario)
