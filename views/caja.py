0ebdf9a23dfa1481a2fc90fad27a485a0b6a5a77
copymary9170 <copymary9170@gmail.com>
2026-03-08 22:27:46 -0400

    Update caja.py

--- views/caja.py
+++ views/caja.py
@@ -3,6 +3,9 @@ from modules.caja import render_caja as caja_module
 
 
 def render_caja(usuario):
+
     st.title("🏁 Cierre de Caja")
 
-    caja_module(usuario)
+    user_role = st.session_state.get("rol", "Admin")
+
+    caja_module(usuario, user_role)
