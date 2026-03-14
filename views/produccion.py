e6e058533f08a9bc82c088bd0e102bc6a231f734
copymary9170 <copymary9170@gmail.com>
2026-03-14 16:30:05 -0400

    Update produccion.py

--- views/produccion.py
+++ views/produccion.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from modules.produccion import render_produccion as produccion_module
+from modules.produccion import render_produccion_manual as produccion_module
 
 
 def render_produccion(usuario):
     st.title("Producción")
-
     produccion_module(usuario)
