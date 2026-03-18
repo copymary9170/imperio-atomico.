fdc0c9dddf14d7f4a591b13abd4cd31ee21ad1d9
copymary9170 <copymary9170@gmail.com>
2026-03-17 21:28:16 -0400

    Update activos.py

--- views/activos.py
+++ views/activos.py
@@ -1,9 +1,5 @@
-import streamlit as st
 from modules.activos import render_activos as activos_module
 
 
 def render_activos(usuario: str):
-
-    st.title("🏗️ Activos")
-
     activos_module(usuario)
