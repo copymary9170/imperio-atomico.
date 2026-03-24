da71d868eee8d441da91f30be01a2366c4644ce2
copymary9170 <copymary9170@gmail.com>
2026-03-24 10:05:11 -0400

    Create planeacion_financiera.py

---
+++ views/planeacion_financiera.py
@@ -0,0 +1,5 @@
+from modules.planeacion_financiera import render_planeacion_financiera as render_planeacion_financiera_module
+
+
+def render_planeacion_financiera(usuario):
+    render_planeacion_financiera_module(usuario)
