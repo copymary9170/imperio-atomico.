93c43a7ebf88f7205b00405198c6b6c4f28b7c39
copymary9170 <copymary9170@gmail.com>
2026-03-27 09:51:01 -0400

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,17 @@
+from .operacion_industrial import (
+    ClaseActivo,
+    CriticalityScore,
+    EstadoMantenimiento,
+    MaintenanceOrderInput,
+    TipoMantenimiento,
+    TraceabilityEvent,
+)
+
+__all__ = [
+    "ClaseActivo",
+    "CriticalityScore",
+    "EstadoMantenimiento",
+    "MaintenanceOrderInput",
+    "TipoMantenimiento",
+    "TraceabilityEvent",
+]
