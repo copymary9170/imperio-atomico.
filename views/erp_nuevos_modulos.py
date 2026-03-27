04fbd21513eb34edcb1a22390e1884881ea6ac65
copymary9170 <copymary9170@gmail.com>
2026-03-26 21:52:12 -0400

    Create erp_nuevos_modulos.py

---
+++ views/erp_nuevos_modulos.py
@@ -0,0 +1,37 @@
+from modules.erp_nuevos_modulos import render_portafolio_modulos
+from views.compras_proveedores import render_compras_proveedores
+from views.cuentas_por_pagar import render_cuentas_por_pagar
+from views.tesoreria import render_tesoreria
+from views.costeo_industrial import render_costeo_industrial
+from views.mermas_desperdicio import render_mermas_desperdicio
+from views.mantenimiento_activos import render_mantenimiento_activos
+from views.planificacion_produccion import render_planificacion_produccion
+from views.control_calidad import render_control_calidad
+from views.rutas_produccion import render_rutas_produccion
+from views.impuestos import render_impuestos
+from views.conciliacion_bancaria import render_conciliacion_bancaria
+from views.marketing_ventas import render_marketing_ventas
+from views.fidelizacion import render_fidelizacion
+from views.rrhh import render_rrhh
+from views.seguridad_roles import render_seguridad_roles
+from views.manuales_sop import render_manuales_sop
+
+__all__ = [
+    "render_portafolio_modulos",
+    "render_compras_proveedores",
+    "render_cuentas_por_pagar",
+    "render_tesoreria",
+    "render_costeo_industrial",
+    "render_mermas_desperdicio",
+    "render_mantenimiento_activos",
+    "render_planificacion_produccion",
+    "render_control_calidad",
+    "render_rutas_produccion",
+    "render_impuestos",
+    "render_conciliacion_bancaria",
+    "render_marketing_ventas",
+    "render_fidelizacion",
+    "render_rrhh",
+    "render_seguridad_roles",
+    "render_manuales_sop",
+]
