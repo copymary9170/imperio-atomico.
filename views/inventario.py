607965665761233a5d491f817cad19efc43c13e8
copymary9170 <copymary9170@gmail.com>
2026-03-29 15:33:02 -0400

    Update inventario.py

--- views/inventario.py
+++ views/inventario.py
@@ -2,7 +2,5 @@ import streamlit as st
 from modules.inventario import render_inventario as inventario_module
 
 
-def render_inventario(usuario):
-    st.title("📦 Inventario")
-
+def render_inventario(usuario: str) -> None:
     inventario_module(usuario)
