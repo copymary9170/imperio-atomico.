6228faa72e9d02349f30fa00522c920d5d502d1c
copymary9170 <copymary9170@gmail.com>
2026-03-28 22:50:09 -0400

    Update planeacion_financiera.py

--- views/planeacion_financiera.py
+++ views/planeacion_financiera.py
@@ -1,5 +1,9 @@
-from modules.planeacion_financiera import render_planeacion_financiera as render_planeacion_financiera_module
+from __future__ import annotations
+
+from modules.planeacion_financiera import (
+    render_planeacion_financiera as render_planeacion_financiera_module,
+)
 
 
-def render_planeacion_financiera(usuario):
+def render_planeacion_financiera(usuario: str) -> None:
     render_planeacion_financiera_module(usuario)
