2f02a117c4fe0f533b9d338a830103949ef4348e
copymary9170 <copymary9170@gmail.com>
2026-03-31 23:19:04 -0400

    Update Planificación_de_producción.py

--- views/Planificación_de_producción.py
+++ views/Planificación_de_producción.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from modules.Planificación_de_producción import render_produccion as produccion_module
+
+import modules.Planificación_de_producción as planificacion_produccion_module
 
 
 def render_produccion(usuario):
     st.title("🗓️ Planificación de producción")
-    produccion_module(usuario)
+    planificacion_produccion_module.render_produccion(usuario)
