4bbcceaebe417a7e233e7a4dbdfbed7b7d204652
copymary9170 <copymary9170@gmail.com>
2026-03-31 22:20:39 -0400

    Delete views/planificacion_produccion.py

--- views/planificacion_produccion.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from modules.erp_nuevos_modulos import render_module_blueprint
-
-
-def render_planificacion_produccion(usuario):
-    st.title("🗓️ Planificación de producción")
-    render_module_blueprint("planificacion_produccion", usuario)
