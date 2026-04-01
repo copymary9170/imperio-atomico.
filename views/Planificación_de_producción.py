66378f09bcfcd7e59d65c6a3e2fe9d75105ff9fa
copymary9170 <copymary9170@gmail.com>
2026-04-01 00:29:19 -0400

    Update Planificación_de_producción.py

--- views/Planificación_de_producción.py
+++ views/Planificación_de_producción.py
@@ -5,5 +5,3 @@ from modules.produccion import render_produccion as produccion_module
 def render_produccion(usuario):
     st.title("🗓️ Planificación de producción")
     produccion_module(usuario)
-
-
