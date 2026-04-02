45151c0609dede61e15c20e1d9c27a71604c3326
copymary9170 <copymary9170@gmail.com>
2026-04-02 17:23:17 -0400

    Create planificacion_produccion.py

---
+++ views/planificacion_produccion.py
@@ -0,0 +1,8 @@
+ModuleNotFoundError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app).
+Traceback:
+File "/mount/src/imperio-atomico./app.py", line 51, in <module>
+    from views.erp_nuevos_modulos import (
+    ...<17 lines>...
+    )
+File "/mount/src/imperio-atomico./views/erp_nuevos_modulos.py", line 8, in <module>
+    from views.planificacion_produccion import render_planificacion_produccion
