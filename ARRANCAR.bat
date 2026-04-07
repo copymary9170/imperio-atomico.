741667839b21f8de97f2a50325fdda940b6b2b29
copymary9170 <copymary9170@gmail.com>
2026-04-06 20:48:14 -0400

    Update ARRANCAR.bat

--- ARRANCAR.bat
+++ ARRANCAR.bat
@@ -3,7 +3,9 @@ echo ===============================
 echo INICIANDO IMPERIO ERP
 echo ===============================
 
-py -m streamlit run app.py
+REM --server.runOnSave: refresca al guardar
+REM --server.fileWatcherType=auto: detecta cambios de archivos en más entornos
+py -m streamlit run app.py --server.runOnSave true --server.fileWatcherType auto
 
 echo.
 echo Si hubo un error revisa arriba.
