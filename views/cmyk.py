dcc1ed2cd26f89a6693ad78a93ba64f2cedc8f6f
copymary9170 <copymary9170@gmail.com>
2026-05-17 13:48:40 -0400

    Integrar control tecnico en CMYK

--- views/cmyk.py
+++ views/cmyk.py
@@ -1,5 +1,19 @@
+import streamlit as st
+
 from modules.cmyk_engine import render_cmyk as render_cmyk_modulo
+from views.cmyk_control import render_cmyk_control
 
 
 def render_cmyk(usuario):
-    render_cmyk_modulo(usuario)
+    st.title("🎨 CMYK")
+
+    tab_motor, tab_control = st.tabs([
+        "Motor CMYK",
+        "📊 Control CMYK",
+    ])
+
+    with tab_motor:
+        render_cmyk_modulo(usuario)
+
+    with tab_control:
+        render_cmyk_control(usuario)
