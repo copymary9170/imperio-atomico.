8fd3349ffd11627a99790c3bc91f94f1d40d82b9
copymary9170 <copymary9170@gmail.com>
2026-05-17 19:11:25 -0400

    Integrar contadores y clics en CMYK

--- views/cmyk.py
+++ views/cmyk.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 from modules.cmyk_engine import render_cmyk as render_cmyk_modulo
 from views.cmyk_control import render_cmyk_control
+from views.contadores_clics import render_contadores_clics
 
 
 def render_cmyk(usuario):
     st.title("🎨 CMYK")
 
-    tab_motor, tab_control = st.tabs([
+    tab_motor, tab_control, tab_contadores = st.tabs([
         "Motor CMYK",
         "📊 Control CMYK",
+        "🖨️ Contadores y clics",
     ])
 
     with tab_motor:
@@ -17,3 +19,6 @@ def render_cmyk(usuario):
 
     with tab_control:
         render_cmyk_control(usuario)
+
+    with tab_contadores:
+        render_contadores_clics(usuario)
