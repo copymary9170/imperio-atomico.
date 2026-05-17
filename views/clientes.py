51d7be4b26f4993c98c0600307b73c207b213d1e
copymary9170 <copymary9170@gmail.com>
2026-05-17 12:42:22 -0400

    Integrar inteligencia comercial en Clientes

--- views/clientes.py
+++ views/clientes.py
@@ -1,8 +1,18 @@
 import streamlit as st
 from modules.clientes import render_clientes as clientes_module
+from views.clientes_inteligencia import render_clientes_inteligencia
 
 
 def render_clientes(usuario):
     st.title("👥 Clientes")
 
-    clientes_module(usuario)
+    tab_operativo, tab_inteligencia = st.tabs([
+        "Clientes operativos",
+        "🧠 Inteligencia comercial",
+    ])
+
+    with tab_operativo:
+        clientes_module(usuario)
+
+    with tab_inteligencia:
+        render_clientes_inteligencia(usuario)
