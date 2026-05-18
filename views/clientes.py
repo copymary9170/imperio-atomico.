58dcfab6fa5de165338bd660f3cb77079abc227a
copymary9170 <copymary9170@gmail.com>
2026-05-18 11:45:03 -0400

    Integrar fidelizacion dentro de Clientes

--- views/clientes.py
+++ views/clientes.py
@@ -2,15 +2,17 @@ import streamlit as st
 from modules.clientes import render_clientes as clientes_module
 from views.clientes_inteligencia import render_clientes_inteligencia
 from views.crm_avanzado import render_crm_avanzado
+from views.erp_nuevos_modulos import render_fidelizacion
 
 
 def render_clientes(usuario):
     st.title("👥 Clientes")
 
-    tab_crm, tab_operativo, tab_inteligencia = st.tabs([
+    tab_crm, tab_operativo, tab_inteligencia, tab_fidelizacion = st.tabs([
         "🤝 CRM / Prospectos",
         "Clientes operativos",
         "🧠 Inteligencia comercial",
+        "⭐ Fidelización",
     ])
 
     with tab_crm:
@@ -21,3 +23,6 @@ def render_clientes(usuario):
 
     with tab_inteligencia:
         render_clientes_inteligencia(usuario)
+
+    with tab_fidelizacion:
+        render_fidelizacion(usuario)
