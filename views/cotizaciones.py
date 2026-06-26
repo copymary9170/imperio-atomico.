2ff938fae9e63f95d5948a0e219f4bc6abdf4ce8
copymary9170 <copymary9170@gmail.com>
2026-06-25 20:26:31 -0400

    feat: make file analysis the first quote workflow

--- views/cotizaciones.py
+++ views/cotizaciones.py
@@ -2,11 +2,18 @@ import streamlit as st
 
 from modules.cotizaciones import render_cotizaciones as cotizaciones_module
 from views.costeo_integral import render_costeo_integral
+from views.cotizador_archivo import render_cotizador_archivo
 
 
 def render_cotizaciones(usuario):
     st.title("📝 Cotizaciones")
-    tab_integral, tab_legacy = st.tabs(["🔗 Costeo integral", "Cotizaciones actuales"])
+    tab_file, tab_integral, tab_legacy = st.tabs([
+        "📄 Cotizar desde archivo",
+        "🔗 Costeo integral",
+        "Cotizaciones actuales",
+    ])
+    with tab_file:
+        render_cotizador_archivo(usuario)
     with tab_integral:
         render_costeo_integral(usuario)
     with tab_legacy:
